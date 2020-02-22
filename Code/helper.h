08e429cb5e8f4517acf6e9c39ae2048ff620cb67
forewing <jujianai@hotmail.com>
2020-02-22 22:10:54 +0800

    Add comment support

--- Code/helper.h
+++ Code/helper.h
@@ -1,16 +1,25 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#include <stdbool.h>
 #include <stdio.h>
 
 #define STRING_OF(__STRING_ARG__) #__STRING_ARG__
 
-// #define DEBUG
+#define DEBUG
 
 #ifdef DEBUG
 #define LOG(...) printf(__VA_ARGS__);
 #else
 #define LOG(...) ;
 #endif
+
+#define lex_error(__msg__)                                             \
+    fprintf(stderr, "Error type A at Line %d: %s \"%s\".\n", yylineno, \
+            __msg__, yytext);
+
+#define syn_error(__msg__)                                             \
+    fprintf(stderr, "Error type B at Line %d: %s \"%s\".\n", yylineno, \
+            __msg__, yytext);
 
 #endif
