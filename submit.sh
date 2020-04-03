3cf61782a8011188a34a6db2f9e67566f51bb303
forewing <jujianai@hotmail.com>
2020-04-03 19:26:47 +0800

    Add submit script

---
+++ submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -rf Lab/*
+mkdir -p Lab
+
+(cd Code && make parser)
+mv Code/parser Lab/parser
+
+cp report.pdf Lab/
+
+(cd Code && make clean)
+cp -r Code Lab/
+rm -rf Lab/Code/compilers-tests
+
+mkdir -p Lab/Test
+cp -r Test/Lab1 Lab/Test
+cp -r Test/Lab2 Lab/Test
+
+cp README Lab/
+
+if [[ -z "${STUID}" ]]; then
+    TARGET_ZIP="Lab.zip"
+else
+    TARGET_ZIP="${STUID}.zip"
+fi
+
+zip -q -r ${TARGET_ZIP} Lab
+echo "Compressed into ${TARGET_ZIP}"
