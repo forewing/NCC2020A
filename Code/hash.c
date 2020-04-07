7c2a1d9411e9ca8678f6419acf4892d5a7f02242
forewing <jujianai@hotmail.com>
2020-04-07 14:21:15 +0800

    Add HashMap

---
+++ Code/hash.c
@@ -0,0 +1,98 @@
+#include "hash.h"
+
+unsigned int hash(const char* str) {
+    unsigned int hash = 5381;
+    int c;
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    return hash % HASH_SIZE;
+}
+
+HashMap* hashmap_new() {
+    HashMap* ret = (HashMap*)malloc(sizeof(HashMap));
+    for (int i = 0; i < HASH_SIZE; i++)
+        ret->nodes[i] = NULL;
+    return ret;
+}
+
+void hashmap_free(HashMap* map) {
+    for (int i = 0; i < HASH_SIZE; i++) {
+        HashNode* ptr = map->nodes[i];
+        while (ptr) {
+            HashNode* tmp = ptr;
+            ptr = ptr->next;
+            HASHNODE_FREE(tmp);
+        }
+    }
+}
+
+int hashmap_insert(HashMap* map, const char* key, SymbolNode* data) {
+    if (hashmap_node(map, key) != NULL)
+        return -1;  // Duplicated
+
+    unsigned int pos = hash(key);
+
+    HashNode* newNode = (HashNode*)malloc(sizeof(HashNode));
+    newNode->data = data;
+    newNode->key = strdup2(key);
+    newNode->next = map->nodes[pos];
+    map->nodes[pos] = newNode;
+
+    return 0;
+}
+
+HashNode* hashmap_node(HashMap* map, const char* key) {
+    unsigned int pos = hash(key);
+    HashNode* ptr = map->nodes[pos];
+
+    while (ptr) {
+        if (!strcmp(key, ptr->key))
+            return ptr;
+        ptr = ptr->next;
+    }
+
+    return NULL;
+}
+
+SymbolNode* hashmap_value(HashMap* map, const char* key) {
+    HashNode* node = hashmap_node(map, key);
+    if (node == NULL)
+        return NULL;
+    return node->data;
+}
+
+int hashmap_delete(HashMap* map, const char* key) {
+    unsigned int pos = hash(key);
+    HashNode* ptr = map->nodes[pos];
+
+    HashNode* node = hashmap_node(map, key);
+    if (node == NULL)
+        return -1;
+
+    if (node == ptr) {
+        map->nodes[pos] = ptr->next;
+        HASHNODE_FREE(node);
+        return 0;
+    }
+
+    while (ptr->next) {
+        if (ptr->next == node) {
+            ptr->next = node->next;
+            HASHNODE_FREE(node);
+            return 0;
+        }
+        ptr = ptr->next;
+    }
+
+    return -1;
+}
+
+void hashmap_print(HashMap* map) {
+    for (int i = 0; i < HASH_SIZE; i++) {
+        HashNode* ptr = map->nodes[i];
+        while (ptr) {
+            printf("%d\t%s\n", i, ptr->key);
+            ptr = ptr->next;
+        }
+    }
+}
---
+++ Code/hash.h
@@ -0,0 +1,39 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "helper.h"
+#include "symbol.h"
+
+#define HASH_SIZE 24593
+
+typedef struct HashNode {
+    char* key;
+    SymbolNode* data;
+    struct HashNode* next;
+} HashNode;
+
+typedef struct HashMap {
+    HashNode* nodes[HASH_SIZE];
+    struct HashMap* next;
+} HashMap;
+
+unsigned int hash(const char* str);
+HashMap* hashmap_new();
+void hashmap_free(HashMap* map);
+int hashmap_insert(HashMap* map, const char* key, SymbolNode* data);
+int hashmap_delete(HashMap* map, const char* key);
+HashNode* hashmap_node(HashMap* map, const char* key);
+SymbolNode* hashmap_value(HashMap* map, const char* key);
+void hashmap_print(HashMap* map);
+
+#define HASHNODE_FREE(__NODE__)    \
+    {                              \
+        FREE(__NODE__->key);       \
+        type_free(__NODE__->data); \
+        FREE(__NODE__);            \
+    }
+
+#endif
