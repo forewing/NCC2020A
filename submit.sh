782d2e8ee72afba73ae1cf65cecdb3c6413a992c
forewing <jujianai@hotmail.com>
2020-05-04 23:27:50 +0800

    update submit script

--- submit.sh
+++ submit.sh
@@ -10,7 +10,8 @@ cp report.pdf Lab/
 
 (cd Code && make clean)
 cp -r Code Lab/
-rm -rf Lab/Code/compilers-tests
+cp Makefile_official Lab/Code/Makefile
+rm -rf Lab/Code/parser-gdb*
 
 mkdir -p Lab/Test
 cp -r Test/Lab1 Lab/Test
@@ -18,10 +19,10 @@ cp -r Test/Lab2 Lab/Test
 
 cp README Lab/
 
-if [[ -z "${STUID}" ]]; then
+if [[ -z "$1" ]]; then
     TARGET_ZIP="Lab.zip"
 else
-    TARGET_ZIP="${STUID}.zip"
+    TARGET_ZIP="$1.zip"
 fi
 
 rm -rf *.zip
